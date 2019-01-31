aeed439b70d84c7fbb81637050ffbb195ce2ff2f
TitusKirch <tituskirch1999@gmail.com>
2019-02-01 00:44:21 +0100

    Add basic user model

---
+++ models/user/User.py
@@ -0,0 +1,17 @@
+from app import database
+
+
+class User(database.Model):
+
+    # model settings
+    __tablename__ = 'Users'
+
+    # model setup
+    id = database.Column(database.BIGINT, primary_key=True)
+    username = database.Column(database.String(32), unique=True, nullable=False)
+    surname = database.Column(database.String(64), nullable=False)
+    first_name = database.Column(database.String(64), nullable=False)
+    email = database.Column(database.String(132), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User[ID: %r | username: %r]>' % (self.id, self.username)
