3013e286e9d9274a9376b0b954a52cff097ac3b4
TitusKirch <tituskirch1999@gmail.com>
2019-02-01 13:53:46 +0100

    Distinguish between main and test routes and enter example index template

--- routes.py
+++ routes.py
@@ -1,12 +1,19 @@
 from app import app
 from models.user import *
+from flask import render_template
 
 
+###########################################
+# MAIN ROUTES                             #
+###########################################
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
+###########################################
+# TEST ROUTES                             #
+###########################################
 @app.route('/user/<int:user_id>')
 def user(user_id):
     selected_user = User.query.filter_by(id=user_id).first_or_404()
